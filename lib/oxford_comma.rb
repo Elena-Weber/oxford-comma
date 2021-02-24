b7ff6c7fc619dfd96d09277be530bc2e4ff97ac5
Elena-Weber <crochetmiracles@gmail.com>
2021-02-24 16:21:22 -0500

    commit tree e11d23bcd4ece3e98d1f74b4f8833c956faeb2d7

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        list = array.join(" and ")
+    elsif array.length >= 3
+        list = array
+        list[-1] = "and #{list[-1]}"
+        list = list.join(", ")
+    else
+        list = array.join
+    end
+    return list
 end
